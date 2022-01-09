7b76c01ac4691242cfb4aa5a02462ce20756ef25
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2022-01-09 16:04:43 -0300

    version 2, recommended

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,43 +1,43 @@
 package application;
 
+import java.util.HashSet;
 import java.util.Locale;
 import java.util.Scanner;
-
-import model.entitie.Student;
+import java.util.Set;
 
 public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Student student = new Student();
+		Set<Integer> students = new HashSet<Integer>();
 		
-		
-		System.out.print("How many course: ");
+		System.out.print("How many students for course A? ");
 		int n = sc.nextInt();
 		
-		System.out.print ("How many students");
-		int M = sc.nextInt();
-		
-		for(int i = 1; i<= n; i++) {
-			System.out.println("couse #" + i);
-			for(int j = 1; j <= M; j++) {
-				System.out.print("number: ");
-				int number = sc.nextInt();
-				sc.nextLine();
-				System.out.print("Name: ");
-				String name = sc.nextLine();
-				System.out.print("Teacher's course:");
-				String teachersCourse = sc.nextLine();
-				System.out.print("Couser: ");
-				char course = sc.next().charAt(0);
-				student.getStudant().add(new Student(number, name, teachersCourse, course));				
-			}
+		for(int i = 1; i <= n; i++) {
+			int number = sc.nextInt();
+			students.add(number);
 		}
 		
-		for(Student st : student.getStudant()) {
-			System.out.println(st.getNumber());			
-		}		
+		System.out.print("How many students for couse B? ");
+		int m  = sc.nextInt();
+		
+		for (int i = 1; i<= m; i++) {
+			int number = sc.nextInt();
+			students.add(number);
+		}
+		
+		System.out.print("How many students for couse C? ");
+		int x = sc.nextInt();
+		
+		for(int i =1; i <= x; i++) {
+			int number = sc.nextInt();
+			students.add(number);
+		}
+		
+		System.out.println("Total students: " + students.size());
+		
 		sc.close();
 	}
 
